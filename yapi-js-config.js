5a10c255ddc87cc4846de53e404f94a6c1f924d5
spidergod <spidergod23@gmail.com>
2021-03-04 10:25:31 +0800

    新規則 function name MAKER

--- yapi-js-config.js
+++ yapi-js-config.js
@@ -10,29 +10,29 @@ try {
   loadJson.d = process.env.npm_lifecycle_event.split(':');
 }
 
-const server = loadJson.server || 'https://wyapi.hw.codes';
+const server = loadJson.server || '127.0.0.1:3030';
+
 const token = loadJson.token || 'ab6d3e94bd7e990d40cc50b1876e8551d0735090c8fc912d07bad592a54aa1df';
+
 const projectId = loadJson.id || '73';
 
-const globalCode = '';
-
 const methodName = (apipath, method) => {
-  const apipaths = apipath.replace(/-/g, '/').split('/').filter(Boolean);
-  let name = [];
-  name.push(method.toLowerCase());
+  const apipaths = apipath.replace(/:(\w+)/g, 'by-$1').replace(/{\w+}/g, 'by-$1').replace(/[-_]/g, '/').split('/').filter(Boolean);
   const filterString = ['{', '}', 'api', 'v1', 'mock', ':'];
   
+  let name = [];
+  
   if (apipaths.length > 1) {
-    name.api.Paths.reduce((acc, path) => {
+    name = apipaths.reduce((acc, path) => {
       filterString.some(str => path.includes(str)) || acc.push(path);
       return acc;
     }, []);
   } else if (apipaths.length === 1) {
-    name = [].concat(name, [apipaths[0]]);
+    name = [].concat(name, [ apipaths[0] ]);
   }
   name = name.map(str => {
     let tempStr = '';
-    // eslint-disable-next-line no-useless-escape
+   
     tempStr = str.replace(/[^a-zA-Z0-9\_]+/g, '');
     tempStr = tempStr.substr(0, 1).toUpperCase() + tempStr.substr(1);
     return tempStr;
