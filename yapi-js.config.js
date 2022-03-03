abd4e43c6da8ee28b98fe10f955b8e6ff0d20f3f
waitting <spidergod23@gamil.com>
2022-03-03 23:27:12 +0800

    [feat] yapi test

--- yapi-js.config.js
+++ yapi-js.config.js
@@ -1,45 +1,55 @@
 const path = require('path');
 
-const server = 'https://wyapi.hw.codes';
-const token = 'ab6d3e94bd7e990d40cc50b1876e8551d0735090c8fc912d07bad592a54aa1df';
-const projectId = '73';
+let loadJson = {};
 
-const globalCode = '';
+try {
+  loadJson = require(path.resolve(process.cwd(), `src/codegen/${process.env.npm_lifecycle_event.split(':').pop()}_json2service.json` ));
+  const url = new URL(loadJson.remoteUrl);
+  loadJson.token = url.searchParams.get('token');
+  loadJson.token = url.searchParams.get('pid');
+  loadJson.token = url.origin;
+  loadJson.d = process.env.npm_lifecycle_event.split(':');
+} catch (e){
+
+}
+
+const server = loadJson.server || 'http://localhost:40001';
+
+const token = loadJson.token || '97aa3627b0bb1f4f4980ad2b36a99439fa280fab3028873ec260f1dd91c934b1';
+
+const projectId = loadJson.id || '11';
 
 const methodName = (apipath, method) => {
-  // console.log('api name: ', apipath);
-  const apipaths = apipath.replace(/-/g, '/').split('/');
+  const apipaths = apipath.replace(/:(\w+)/g, 'by-$1').replace(/{\w+}/g, 'by-$1').replace(/[-_]/g, '/').split('/').filter(Boolean);
+  const filterString = ['{',
+    '}',
+    'api',
+    'v1',
+    'mock',
+    ':'];
+
   let name = [];
-  name.push(method.toLowerCase());
+
   if (apipaths.length > 1) {
-    for (let i = 1; i <= apipaths.length; i++) {
-      if (
-        apipaths.length - i > 1 &&
-        !apipaths[i + 1].includes('{') &&
-        !apipaths[i + 1].includes('}') &&
-        !apipaths[i + 1].includes('deleted') &&
-        !apipaths[i + 1].includes(projectId) &&
-        !apipaths[i + 1].includes('mock')
-      ) {
-        name.push(apipaths[i + 1]);
-      }
-    }
-    // console.log('apipaths', apipaths);
-    // console.log('name', name);
+    name = apipaths.reduce((acc, path) => {
+      filterString.some(str => path.includes(str)) || acc.push(path);
+      return acc;
+    }, []);
   } else if (apipaths.length === 1) {
     name = [].concat(name, [apipaths[0]]);
   }
-  name = name.map((str, index) => {
+  name = name.map(str => {
     let tempStr = '';
-    // eslint-disable-next-line no-useless-escape
+
     tempStr = str.replace(/[^a-zA-Z0-9\_]+/g, '');
-    if (index !== 0) {
-      tempStr = tempStr.substr(0, 1).toUpperCase() + tempStr.substr(1);
-    }
+    tempStr = tempStr.substr(0, 1).toUpperCase() + tempStr.substr(1);
     return tempStr;
   });
+  name = [method.toLowerCase(), ...name];
   return name.join('');
 };
+
+const globalCode = '';
 
 const template = '';
 
@@ -47,7 +57,7 @@ const moduleMode = true;
 
 const distFile = null;
 
-const distFolder = path.resolve(process.cwd(), 'src/api');
+const distFolder = path.resolve(process.cwd(), process.env.API_GEN_FOLDER || 'src/apiService');
 
 const useCustomHttpRequest = true;
 
