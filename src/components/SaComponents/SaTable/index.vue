5474cbbd4d3821fd899ada95600af275916c737f
waitting <spidergod23@gamil.com>
2023-12-29 01:07:04 +0800

    [feat] set init satable

--- src/components/SaComponents/SaTable/index.vue
+++ src/components/SaComponents/SaTable/index.vue
@@ -1,18 +1,30 @@
 <template>
-  <span>234</span>
+  <div class="saTable">
+    <span>1234567890-</span>
+  </div>
 </template>
 
 <script>
 export default {
   name: "SaTable",
+  components: {},
+  props: {
+
+  },
+  data(){
+    return {
+
+    };
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
 };
 </script>
 
-<style scoped>
-
-
-
-
-
+<style lang="=scss" scoped>
 </style>
 
