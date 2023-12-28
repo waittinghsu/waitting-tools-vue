8e5d014054dce7a715fda961137d0a93ccb767a0
waitting <spidergod23@gamil.com>
2023-12-29 01:04:37 +0800

    [feat] 更新套件

---
+++ src/components/Breadcrumb/test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="d-flex flex-wrap">
+    <div class="">
+      1234
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+};
+</script>
+
+<style scoped>
+
+</style>
