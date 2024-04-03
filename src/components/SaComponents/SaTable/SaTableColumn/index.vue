c8787deda919fa9ff4f1d8168b963eb31a05d4ef
spidergod <spidergod23@gmail.com>
2024-04-03 18:23:51 +0800

    Create index.vue
    this is vue jsx

---
+++ src/components/SaComponents/SaTable/SaTableColumn/index.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  name: 'SaTableColumn',
+  inject: {
+    pageProvideComponent: {
+      default: {},
+    },
+  },
+  props: {
+    attrs: {
+      type: Object,
+      default: () => ({}),
+      required: true,
+    },
+  },
+  render(h) {
+    const { label: labelTemp, render: renderFunction, ...other } = this.attrs;
+    const label = typeof labelTemp === 'function' ? labelTemp() : labelTemp;
+
+    const attrs = { ...other, label };
+    const scopedSlots = {};
+    if (renderFunction) {
+      scopedSlots.default = (scope) => renderFunction(h, scope, this);
+    }
+    return h(
+      'el-table-column',
+      {
+        attrs,
+        scopedSlots,
+      },
+      // attrs.isGroupHeader ? [attrs.render(h)] : null,
+    );
+  },
+};
+</script>
