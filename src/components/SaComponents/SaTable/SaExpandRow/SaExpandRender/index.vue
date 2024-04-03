0cdf6a7a6e158081c0713b7da6d96c3698a4ad76
spidergod <spidergod23@gmail.com>
2024-04-03 18:37:37 +0800

    Create index.vue
    the expand render jsx

---
+++ src/components/SaComponents/SaTable/SaExpandRow/SaExpandRender/index.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  name: 'SaExpandRender',
+  props: {
+    attrs: {
+      type: Object,
+      default: () => ({}),
+      required: true,
+    },
+  },
+  data() {
+    return {
+      aaa: {},
+    };
+  },
+  render(h) {
+    const { col = {}, row } = this.attrs;
+    const label = typeof col?.label === 'function' ? col.label() : col?.label;
+    // const attrs = { ...omit(col, ['expandRender']), label };
+    // const scopedSlots = {};
+    // console.log(1, label, this.attrs, (scope) => attrs.render(h, scope, this));
+    // if (attrs.render) {
+    //   scopedSlots.default = (scope) => attrs.render(h, scope, this);
+    // }
+    return h(
+      'div',
+      { class: 'd-flex flex-row' },
+      [
+        h('span', { class: 'exp-name' }, `${label}:`),
+        col.expandRender ? col.expandRender(h, { row, column: col }, this) : '',
+      ],
+    );
+  },
+};
+</script>
