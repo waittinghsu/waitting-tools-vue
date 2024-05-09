cfa6a964111d3414473cc44f9b8152ac76bf7d93
spidergod <spidergod23@gmail.com>
2024-05-09 13:39:45 +0800

    Update utility index.js
    常用的方法抽出来

--- src/utils/index.js
+++ src/utils/index.js
@@ -178,3 +178,155 @@ export function pureDebounce(fn, delay = 1000) {
   }
   return closure;
 }
+
+
+// 域名規則檢測
+export function checkDomain(str) {
+  const reg = new RegExp('^((?!-)[A-Za-z0-9-\u4e00-\u9fa5]{1,63}(?<!-)\\.)+[A-Za-z]{2,6}');
+  return !!str.match(reg);
+}
+
+/**
+ *  創建密碼亂數
+ * @param length 密碼長度
+ * @param chart 可產生的字符
+ * @returns {string}
+ */
+const CHART_STRING = 'abcdefghijklmonpqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+export function makeRandomString(length = 16, chart = CHART_STRING) {
+  let res = '';
+  for (let count = 0, n = chart.length; count < length; ++count) {
+    res += chart.charAt(Math.floor(Math.random() * n));
+  }
+  return res;
+}
+/**
+ * 遞迴重新產生 menus 結構
+ * @param {string} menus 菜單陣列
+ * @param {string} id 父節點代號
+ * @param {number} level 層級
+ * @returns {Array}
+ */
+export function formatMenus(menus, id, level = 0) {
+  const output = [];
+  if (menus && menus.length > 0) {
+    level += 1;
+    menus.forEach((menuInfo) => {
+      const key = menuInfo?.key || menuInfo?.key1 || 'menu-error';
+      const nowId = `${id}-${key}`;
+      // sso全權限的回傳key為 children :::::: 域名系統權限的回傳key為button_children
+      const originChildren = menuInfo?.children || menuInfo?.button_children;
+      const permission = originChildren?.map((item) => (item?.key || item?.key1)) || [];
+      // 遞迴處理 子層菜單
+      const children = formatMenus(originChildren, nowId, level);
+      // child菜單的權限key （字串分割 pid 根據level 層級組合成 （父－本層） ex: root-mainPage-page-bulletin-button => bulletin-button
+      const childPermission = children.map((o) => `${o.pid}-${o.key}`?.split('-')?.splice(level + 1)?.join('-'));
+      // console.log(id, level, menuInfo.name, Array.from(new Set([...permission, ...childPermission])));
+      // console.log(nowId, key, menuInfo.name, `le:${children.length}`, children);
+      /**
+       * 判斷是否為按鈕
+       * １．檢查 key 是否有 action operational
+       * ２．檢查 key 在字首
+       * @param {Object} child
+       * @param {Object} child.key 唯一值 有些系統 為key1
+       * @return {boolean}
+       */
+      function isButton(child) {
+        // console.log(child.name, child.type, child);
+        const childKey = child?.key || child?.key1 || 'menu-error';
+        const buttonKey = ['action', 'operational'];
+        let flag = false;
+        buttonKey.forEach((key) => {
+          childKey.indexOf(key) === 0 && (flag = true);
+        });
+        return flag;
+      }
+
+      output.push(
+        {
+          key,
+          name: menuInfo.name,
+          sort: menuInfo?.sort || 0,
+          id: nowId, // 结点ID
+          pid: id, // 父结点ID
+          level, // 层次
+          vueRouterRedirect: false, // 判斷路由是否有轉址
+          permissions: Array.from(new Set([...permission, ...childPermission])), // 包含的所有子層次的權限
+          childrenLength: originChildren?.length || 0, // 孩子的數量
+          childrenPageLength: originChildren?.filter((child) => !isButton(child))?.length || 0, // 子頁面的數量
+        },
+        ...children,
+      );
+    });
+  }
+  return output;
+}
+
+/**
+ * 遞迴重新產生 route 結構
+ * @param {string} routes 路由列表
+ * @param {string} id 父節點代號
+ * @param {string} parentPath 路由url
+ * @returns {Array}
+ */
+export function formatRoute(routes, id, parentPath) {
+  const output = [];
+  if (routes && routes.length > 0) {
+    routes.forEach((route) => {
+      const nId = `${id}-${route.name}`;
+      // 父路'尾巴'與子路徑'頭'有無斜線 做路徑串接
+      const slash = route.path.startsWith('/') || parentPath.endsWith('/') ? '' : '/';
+      const path = `${parentPath}${slash}${route.path}`;
+      output.push(
+        {
+          path,
+          id: nId,
+          meta: route.meta,
+          name: route.name,
+          hidden: route.hidden,
+          authory: route.authory,
+          redirect: !!route?.redirect,
+        },
+        ...formatRoute(route.children, nId, path),
+      );
+    });
+  }
+  return output;
+}
+
+/**
+ * 遞迴建立菜單２元樹
+ * @param {array} topParentList 菜單列表 樹的頂端
+ * @param {array} menuList 菜單列表 （水平資料）
+ * @param {string} parentKey 父節點代號
+ * @param {string} mainId 父節點代號
+ * @returns {Array}
+ */
+export function buildBinaryTree(topParentList = [], menuList = [], parentKey = 'parent_id', mainId = 'id') {
+  const temp = [];
+  topParentList.forEach((menu) => {
+    const getChildren = menuList.filter((item) => item[parentKey] === menu[mainId]);
+    temp.push({ ...menu, children: [...buildBinaryTree(getChildren, menuList, parentKey, mainId)] });
+  });
+  return temp;
+}
+
+/**
+ * 攤平２元樹
+ * 將children 內的物件攤分出來
+ * @param menus 菜單
+ * @returns {*[]}
+ */
+export function flattenBinaryTree(menus) {
+  const res = [];
+  menus.forEach((item) => {
+    if (item?.children?.length > 0) {
+      res.push({ ...item }, ...flattenBinaryTree(item.children));
+    } else {
+      res.push({ ...item });
+    }
+  });
+  return res;
+}
+
+
