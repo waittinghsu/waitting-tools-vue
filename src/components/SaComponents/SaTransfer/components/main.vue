cb280fe22f008525fa2a91a691781e07764c79cf
spidergod <spidergod23@gmail.com>
2024-05-09 13:33:34 +0800

    Update main.vue
    穿梭匡 主物件

--- src/components/SaComponents/SaTransfer/components/main.vue
+++ src/components/SaComponents/SaTransfer/components/main.vue
@@ -51,6 +51,7 @@ import Emitter from 'element-ui/src/mixins/emitter';
 import Locale from 'element-ui/src/mixins/locale';
 import Migrating from 'element-ui/src/mixins/migrating';
 import TransferPanel from './transfer-panel.vue';
+
 // let start = '';
 export default {
   name: 'ElTransfer',
@@ -63,6 +64,16 @@ export default {
   mixins: [Emitter, Locale, Migrating],
 
   props: {
+    /**
+     * 数据数组。
+     * @typedef {Object} DataObject
+     * @property {string|number} label - 对象的标签（参考 prop 的 label 属性）
+     * @property {string|number} key - 对象的键（参考 prop 的 key 属性）
+     * @property {...any} [otherAttrs] - 其他属性（如果有）
+     */
+    /**
+     * @type {DataObject[]}
+     */
     data: {
       type: Array,
       default() {
@@ -85,7 +96,10 @@ export default {
       type: String,
       default: '',
     },
-    filterMethod: Function,
+    filterMethod: {
+      type: Function,
+      default: undefined,
+    },
     leftDefaultChecked: {
       type: Array,
       default() {
@@ -98,7 +112,25 @@ export default {
         return [];
       },
     },
-    renderContent: Function,
+    /**
+     * @callback RenderContentFunction
+     * @param {Function} h - Vue 的 createElement 函数，用于创建 VNode。
+     * @param {Object} option - 渲染选项对象，包含从 prop 中获取的 attrs 数据。
+     * @param {Object[]} option.data - 渲染选项对象中的 data 属性，类型为数组。
+     * @param {...any} [otherArgs] - 其他参数（如果有）。
+     */
+    /**
+     * 渲染内容的函数。
+     * @type {RenderContentFunction}
+     * @default undefined
+     */
+    renderContent: {
+      type: Function,
+      default: undefined,
+    },
+    /**
+     * @param {Number[]} value
+     */
     value: {
       type: Array,
       default() {
@@ -112,6 +144,15 @@ export default {
       },
     },
     filterable: Boolean,
+    /**
+     * @typedef {Object} PropsObject
+     * @property {string} label - 選項文案属性
+     * @property {string} key - 键属性
+     * @property {string} disabled - 狀態属性
+     */
+    /**
+     * @type PropsObject
+     */
     props: {
       type: Object,
       default() {
@@ -140,33 +181,33 @@ export default {
       const { key } = this.props;
       return this.data.reduce((o, cur) => (o[cur[key]] = cur) && o, {});
     },
-
+    /**
+     * 左邊資料
+     * 原生是使用 indexOf 來對每次對 this.value 遍歷查找 時間複雜度為  O(n * m)
+     * 原生 -> return this.data.filter(item => this.value.indexOf(item[this.props.key]) === -1);
+     * 修改後  O(n + m)
+     * @returns {DataObject[]}
+     */
     sourceData() {
-      const valueObj = {};
-      this.value.forEach((item, index) => {
-        valueObj[item] = true;
-      });
-      return this.data.filter(
-        (item) => !valueObj[item[this.props.key]],
-      );
+      // 這裡做了 字典模式 配置成 key: Boolean 來確認誰不在左邊 this.value 可以想成是右邊的資料
+      const valueDic = this.value.reduce((acc, key) => ({ ...acc, [key]: true }), {});
+      // valueObj 字典查找速度更快
+      return this.data.filter((item) => !valueDic[item[this.props.key]]);
     },
-
+    /**
+     * 右邊資料
+     * @returns {*|DataObject[]}
+     */
     targetData() {
       if (this.targetOrder === 'original') {
-        const valueObj = {};
-        this.value.forEach((item, index) => {
-          valueObj[item] = true;
-        });
-        const data = this.data.filter(
-          (item) => valueObj[item[this.props.key]],
-        );
-        return data;
+        // 這裡做了 字典模式 配置成 key: Boolean 來確認誰不在左邊 this.value 可以想成是右邊的資料
+        const valueDic = this.value.reduce((acc, key) => ({ ...acc, [key]: true }), {});
+        // valueObj 字典查找速度更快
+        return this.data.filter((item) => valueDic[item[this.props.key]]);
       }
       return this.value.reduce((arr, cur) => {
         const val = this.dataObj[cur];
-        if (val) {
-          arr.push(val);
-        }
+        val && arr.push(val);
         return arr;
       }, []);
     },
